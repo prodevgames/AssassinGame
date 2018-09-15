a3615d065cbbfc7859fd367a5808f90e85a82b88
Garrett Hansen <kirypto@gmail.com>
2018-09-15 15:27:13 -0600

    Added return type hints to anonymous helper methods

--- test/test_helper/anon.py
+++ test/test_helper/anon.py
@@ -7,23 +7,23 @@ from assassin_game_csss.domain.player import Player
 from assassin_game_csss.domain.game import Game
 
 
-def anon_string(count: int = 10):
+def anon_string(count: int = 10) -> str:
     return ''.join(choices(ascii_letters + digits, k=count))
 
 
-def anon_player():
+def anon_player() -> Player:
     return Player(anon_string())
 
 
-def anon_location():
+def anon_location() -> Location:
     return Location(anon_string())
 
 
-def anon_item():
+def anon_item() -> Item:
     return Item(anon_string())
 
 
-def anon_game(players: set = None, items: set = None, locations: set = None):
+def anon_game(players: set = None, items: set = None, locations: set = None) -> Game:
     if players is None:
         players = {anon_player(), anon_player(), anon_player()}
     if items is None:
