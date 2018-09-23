c053c00dbb9edd579240e23a0afab8ec6cb82399
Garrett Hansen <kirypto@gmail.com>
2018-09-22 20:23:40 -0600

    Added type hints to Game methods

--- assassin_game_csss/domain/game.py
+++ assassin_game_csss/domain/game.py
@@ -2,6 +2,7 @@ from uuid import UUID
 
 from assassin_game_csss.domain.player import Player
 from assassin_game_csss.domain.target import Target
+from assassin_game_csss.domain.game_state import GameState
 
 
 class Game:
@@ -11,10 +12,10 @@ class Game:
     __scores: dict = None
     __targets: dict = None
 
-    def __init__(self, players: set, items: set, locations: set, num_targets: int = 1):
+    def __init__(self, players: set, items: set, locations: set, num_targets: int = 1) -> None:
         raise NotImplementedError
 
-    def get_status(self):
+    def get_status(self) -> GameState:
         raise NotImplementedError
 
     def get_score(self, player) -> int:
@@ -23,14 +24,14 @@ class Game:
     def get_target(self, player: Player) -> Target:
         raise NotImplementedError
 
-    def start(self):
+    def start(self) -> None:
         raise NotImplementedError
 
-    def end(self):
+    def end(self) -> None:
         raise NotImplementedError
 
     def mark_kill(self, player: Player, target: Target) -> bool:
         raise NotImplementedError
 
-    def get_game_id(self):
+    def get_game_id(self) -> UUID:
         raise NotImplementedError
