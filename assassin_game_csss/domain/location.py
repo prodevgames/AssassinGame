075a9ee79686d1abaa345169d42582c0e0888053
Jarrod Pas <jarrod@jarrodpas.com>
2018-09-24 19:56:12 -0600

    Modified Location to throw exception when name too short

--- assassin_game_csss/domain/location.py
+++ assassin_game_csss/domain/location.py
@@ -2,6 +2,8 @@ class Location:
     __name: str = None
 
     def __init__(self, name: str) -> None:
+        if not name:
+            raise ValueError('name must be at least 1 character')
         raise NotImplementedError
 
     def get_name(self) -> str:
