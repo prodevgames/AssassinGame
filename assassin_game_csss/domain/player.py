c29d23b979d8da7df483538ca3fa6e0d29589297
Garrett Hansen <kirypto@gmail.com>
2018-09-27 17:21:13 -0600

    Fixed bug in type checking of Player name arg

--- assassin_game_csss/domain/player.py
+++ assassin_game_csss/domain/player.py
@@ -2,7 +2,7 @@ class Player:
     __name: str = None
 
     def __init__(self, name: str) -> None:
-        if name is not str:
+        if type(name) is not str:
             raise TypeError("Invalid type given as name to Player")
         if len(name) <= 0:
             raise ValueError("Name cannot be an empty string for Player")
