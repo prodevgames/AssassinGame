0a5709f174357716d60758bf613b1b925ca117e7
Garrett Hansen <kirypto@gmail.com>
2018-10-31 17:32:41 -0600

    Working on modifying Player tests

--- test/domain/test_player.py
+++ test/domain/test_player.py
@@ -1,24 +1,26 @@
-from unittest import TestCase
+from unittest import TestCase, skip
 
 from assassin_game_csss.domain.player import Player
-from test.test_helper.anon import anon_player
+from assassin_game_csss.domain.upid import UPID
+from test.test_helper.anon import anon_player, anon_string, anon_upid
 
 
 class TestPlayer(TestCase):
 
-    def test__constructor__shouldThrowException__whenGivenNonString(self):
+    @skip("Not Yet Implemented")
+    def test__constructor__shouldThrowException__whenGivenNonStringName(self):
         # Act
         # noinspection PyTypeChecker
-        def action_none(): Player(None)
+        def action_none(): Player(None, anon_upid())
 
         # noinspection PyTypeChecker
-        def action_int(): Player(42)
+        def action_int(): Player(42, anon_upid())
 
         # noinspection PyTypeChecker
-        def action_set(): Player(set())
+        def action_set(): Player(set(), anon_upid())
 
         # noinspection PyTypeChecker
-        def action_list(): Player([])
+        def action_list(): Player([], anon_upid())
 
         # Assert
         self.assertRaises(TypeError, action_none)
@@ -26,9 +28,39 @@ class TestPlayer(TestCase):
         self.assertRaises(TypeError, action_set)
         self.assertRaises(TypeError, action_list)
 
-    def test__constructor__shouldThrowException__whenProvidedEmptyString(self):
+    @skip("Not Yet Implemented")
+    def test__constructor__shouldThrowException__whenGivenNeitherUPIDNorStringForIdentifierArgument(self):
         # Act
-        def action(): Player("")
+        # noinspection PyTypeChecker
+        def action_none(): Player(anon_string(), None)
+
+        # noinspection PyTypeChecker
+        def action_int(): Player(anon_string(), 42)
+
+        # noinspection PyTypeChecker
+        def action_set(): Player(anon_string(), set())
+
+        # noinspection PyTypeChecker
+        def action_list(): Player(anon_string(), [])
+
+        # Assert
+        self.assertRaises(TypeError, action_none)
+        self.assertRaises(TypeError, action_int)
+        self.assertRaises(TypeError, action_set)
+        self.assertRaises(TypeError, action_list)
+
+    @skip("Not Yet Implemented")
+    def test__constructor__shouldThrowException__whenProvidedEmptyStringName(self):
+        # Act
+        def action(): Player("", anon_upid())
+
+        # Assert
+        self.assertRaises(ValueError, action)
+
+    @skip("Not Yet Implemented")
+    def test__constructor__shouldThrowException__whenProvidedEmptyStringUPID(self):
+        # Act
+        def action(): Player(anon_string(), "")
 
         # Assert
         self.assertRaises(ValueError, action)
@@ -54,6 +86,32 @@ class TestPlayer(TestCase):
 
         # Assert
         self.assertRaises(AttributeError, action)
+
+    @skip("Not Yet Implemented")
+    def test__upid__shouldReturnUPID__whenConstructedWithStr(self):
+        # Arrange
+        expected_upid_string = str(anon_upid())
+        player = Player(anon_string(), expected_upid_string)
+
+        # Act
+        actual = player.upid
+
+        # Assert
+        self.assertIsInstance(actual, UPID)
+        self.assertEqual(expected_upid_string, str(actual))
+
+    @skip("Not Yet Implemented")
+    def test__upid__shouldReturnUPID__whenConstructedWithUPID(self):
+        # Arrange
+        expected_upid = anon_upid()
+        player = Player(anon_string(), expected_upid)
+
+        # Act
+        actual = player.upid
+
+        # Assert
+        self.assertIsInstance(actual, UPID)
+        self.assertEqual(expected_upid, actual)
 
     def test__equals__shouldReturnTrue__whenConstructionIsIdentical(self):
         # Arrange
