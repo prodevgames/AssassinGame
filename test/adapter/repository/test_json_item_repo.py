64b44fffda0f5d1507f1d6c8e63c6ff4aa4f658f
Garrett Hansen <kirypto@gmail.com>
2018-11-18 21:22:05 -0600

    Tweaked TestJsonItemRepo setUp and tearDown

--- test/adapter/repository/test_json_item_repo.py
+++ test/adapter/repository/test_json_item_repo.py
@@ -1,8 +1,20 @@
+from os import remove
+
 from assassin_game_csss.adapter.repository.json_item_repo import JsonItemRepo
 from test.domain.repository.test_abstract_item_repo import TestAbstractItemRepo
 
 
 class TestInMemoryItemRepo(TestAbstractItemRepo):
+    __json_item_repo_file = "savedItems.json"
+
+    def setUp(self):
+        with open(self.__json_item_repo_file, "w+") as file:
+            file.write("[]")
+        super().setUp()
+
+    def tearDown(self):
+        remove(self.__json_item_repo_file)
+        super().tearDown()
 
     def _get_instance(self):
-        return JsonItemRepo("savedItems.json")
+        return JsonItemRepo(self.__json_item_repo_file)
