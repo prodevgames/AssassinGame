999e9e53a9906987c155353eb9581b160a601a6a
Garrett Hansen <kirypto@gmail.com>
2018-11-28 15:46:33 -0600

    Modified JsonItemRepo to use tempfile mkstemp

--- test/adapter/repository/test_json_item_repo.py
+++ test/adapter/repository/test_json_item_repo.py
@@ -1,20 +1,24 @@
-from os import remove
+from os import remove, close
+from tempfile import mkstemp
 
 from assassin_game_csss.adapter.repository.json_item_repo import JsonItemRepo
 from test.domain.repository.test_abstract_item_repo import TestAbstractItemRepo
 
 
-class TestInMemoryItemRepo(TestAbstractItemRepo):
-    __json_item_repo_file = "savedItems.json"
+class TestJsonItemRepo(TestAbstractItemRepo):
+    __json_item_repo_file_path = None
 
     def setUp(self):
-        with open(self.__json_item_repo_file, "w+") as file:
+        file_descriptor, file_path = mkstemp(prefix="assassinGameTempTestFile_")
+        self.__json_item_repo_file_path = file_path
+        with open(self.__json_item_repo_file_path, "w") as file:
             file.write("[]")
+        close(file_descriptor)
         super().setUp()
 
     def tearDown(self):
-        remove(self.__json_item_repo_file)
+        remove(self.__json_item_repo_file_path)
         super().tearDown()
 
     def _get_instance(self):
-        return JsonItemRepo(self.__json_item_repo_file)
+        return JsonItemRepo(self.__json_item_repo_file_path)
